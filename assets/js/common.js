e609eb571f1baeb0b9e048bfdfd333a48b4a53ae
QM-kyawzinoo <kyawzinoo@qualymm@gmail.com>
2024-09-20 13:11:08 +0630

    Container design changed

--- assets/js/common.js
+++ assets/js/common.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
     $(".accordion-content").css("display","none");
     // $(".accordion-title:first-of-type").addClass("open");
     $(".accordion-title").click(function(){
-     // $(".open").not(this).removeClass("open").next().slideUp(300);
-     $(this).toggleClass("open").next().slideToggle(50);
+     $(".open").not(this).removeClass("open").next().slideUp(300);
+     $(this).toggleClass("open").next().slideToggle(500);
     });
   });
 });
