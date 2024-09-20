6549b6297990a960f02364c84544095bd1fcfa45
QM-kyawzinoo <kyawzinoo@qualymm@gmail.com>
2024-09-20 08:32:05 +0630

    CSS Changed

--- assets/js/common.js
+++ assets/js/common.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     // $(".accordion-title:first-of-type").addClass("open");
     $(".accordion-title").click(function(){
      // $(".open").not(this).removeClass("open").next().slideUp(300);
-     $(this).toggleClass("open").next().slideToggle(200);
+     $(this).toggleClass("open").next().slideToggle(50);
     });
   });
 });
