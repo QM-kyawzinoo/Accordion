35f5cb25c604e303ce0dfa736b5f8b83dcafc74c
QM-kyawzinoo <kyawzinoo@qualymm@gmail.com>
2024-09-24 10:26:20 +0630

    Tab Example

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,8 +1,32 @@
 $(document).ready(function () {
-    $(".accordion-content").css("display","none");
-    // $(".accordion-title:first-of-type").addClass("open");
-    $(".accordion-title").click(function(){
-     $(".open").not(this).removeClass("open").next().slideUp(300);
-     $(this).toggleClass("open").next().slideToggle(500);
-    });
+  $('.nav-list li:first-child').addClass('active');
+  $('.content').hide();
+  $('.content:first').show();
+
+  $('.nav-list li').click(function(){
+    $('.nav-list li').removeClass('active');
+    $(this).addClass('active');
+    $('.content').hide();
+    
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).fadeIn();
+    return false;
   });
+});
+
+
+
+// $('#tabs-nav li:first-child').addClass('active');
+// $('.tab-content').hide();
+// $('.tab-content:first').show();
+
+// // Click function
+// $('#tabs-nav li').click(function(){
+//   $('#tabs-nav li').removeClass('active');
+//   $(this).addClass('active');
+//   $('.tab-content').hide();
+  
+//   var activeTab = $(this).find('a').attr('href');
+//   $(activeTab).fadeIn();
+//   return false;
+// });
